735c2958115646c2ee148055ff566c97d0693356
Amer Hamdan <[amashamdan@gmail.com]>
2015-09-11 23:00:54 +0300

    Optimized index, 2048, webperf, mobile, pizza.html

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      images1: {
+        options: {
+          engine: 'gm',
+          sizes: [{
+            width: '100%',
+            quality: 50
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'img/',
+          dest: 'images/'
+        }]
+      },
+      images2: {
+        options: {
+          engine: 'gm',
+          sizes: [{
+            width: '100%',
+            quality: 50
+          },
+          {
+            width: 100,
+            quality: 50
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'views/images',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    uglify: {
+	    build1: {
+	        src: 'js/perfmatters.js',
+	        dest: 'js/perfmatters.min.js'
+	    },
+	    build2: {
+	    	src: 'views/js/main.js',
+	    	dest: 'views/js/main.min.js' 
+	    },
+	},
+
+	cssmin: {
+	  css1:{
+	    src: 'views/css/style.css',
+	    dest: 'views/css/style.min.css'
+	  },
+	  css2:{
+	    src: 'views/css/bootstrap-grid.css',
+	    dest: 'views/css/bootstrap-grid.min.css'
+	  },
+	  css3:{
+	    src: 'css/print.css',
+	    dest: 'css/print.min.css'
+	  },
+	},
+
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('default', ['cssmin', 'uglify', 'responsive_images']);
+};
